49eed31e935c73df6665544d2a8b558d9d750115
ImNicknamez <136889440+ImNicknamez@users.noreply.github.com>
2023-06-26 15:57:57 +0200

    kk

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ImNicknamez/rize/main/main.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
